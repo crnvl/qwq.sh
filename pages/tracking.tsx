dcfce7e4a49aa4a16a3d2b5a603837a6bbb905a3
angelsflyinhell <azami@namespace.media>
2023-01-11 04:30:36 +0100

    Update tracking.tsx

--- pages/tracking.tsx
+++ pages/tracking.tsx
@@ -8,6 +8,9 @@ const Tracking: NextPage = () => {
         <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
     </svg>)
 
+    const [clicks, setClicks] = useState("N/A")
+    const [lastClicked, setLastClicked] = useState('N/A')
+
     const getTrackingInfo = async () => {
         const data = await fetch('https://qwq.sh/api/track', {
             method: 'POST',
@@ -16,10 +19,18 @@ const Tracking: NextPage = () => {
             })
         })
         const json = await data.json()
-        console.log(json);
+
+        if (json.success) {
+            setClicks(json.clicks)
+            const date = new Date(json.lastClick)
+            const monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ];
+            setLastClicked(`${monthNames[date.getMonth()]} ${date.getDay()}, ${date.getHours()}:${date.getMinutes()}`)
+        }
 
         setIcon(<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
         </svg>)
     }
 
@@ -38,13 +49,13 @@ const Tracking: NextPage = () => {
                     </button>
                 </div>
                 <div className="flex flex-row justify-between gap-4 py-4">
-                    <div className="bg-white m-auto px-8 py-4 rounded-md shadow-lg">
-                        <h1 className="font-bold text-xl text-start">Clicks</h1>
-                        <p className="text-gray-500">12</p>
+                    <div className="bg-white m-auto px-8 py-3 rounded-md shadow-lg">
+                        <h1 className="text-gray-500">Clicks</h1>
+                        <p className="font-bold text-xl text-start">{clicks}</p>
                     </div>
-                    <div className="bg-white m-auto px-8 py-4 rounded-md shadow-lg">
-                        <h1 className="font-bold text-xl text-start">Last clicked</h1>
-                        <p className="text-gray-500">December 12th</p>
+                    <div className="bg-white m-auto px-8 py-3 rounded-md shadow-lg">
+                        <h1 className="text-gray-500">Last clicked</h1>
+                        <p className="font-bold text-xl text-start">{lastClicked}</p>
                     </div>
                 </div>
             </div>
