83a2a861c01618d0322170f69acb82435e941bdf
angelsflyinhell <angelsflyinhell@namespace.media>
2023-01-16 12:25:34 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GHFH4F2G6W"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-GHFH4F2G6W');
+    </script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
