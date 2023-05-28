ab7cdf6cfc2175627bf0d1c4689e921db3206c64
angelsflyinhell <azami@namespace.media>
2023-05-28 14:54:38 +0200

    Fix mongodb connection

--- utils/connect.ts
+++ utils/connect.ts
@@ -1,8 +1,11 @@
-import { connect } from 'mongoose';
+import { connect } from "mongoose";
 
 export const connectDB = async () => {
-    connect(process.env.MONGO_URI || '', (err) => {
-        if (err)
-            throw err;
+  connect(process.env.MONGO_URI || "")
+    .then(() => {
+      console.log("DB Connected!");
     })
-}
+    .catch((err) => {
+      console.log(`DB Connection Error: ${err.message}`);
+    });
+};
